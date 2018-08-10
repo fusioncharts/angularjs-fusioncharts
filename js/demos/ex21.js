12c3f0db1a202893862dc4d518f37776b5b3527e
Rahul9046 <rahul.das@fusioncharts.com>
2018-08-10 17:17:11 +0530

    added responsive chart sample

--- js/demos/ex21.js
+++ js/demos/ex21.js
@@ -11,8 +11,25 @@
 
   app.controller('ex21Controller', function ($scope, $rootScope) {
     $rootScope.demoId = 'ex21';
-    var vm = this;
-    $rootScope.currentTitle = 'Responsive charts';        
+    var vm = this,
+    selectedDimension,
+    dimensionMap = {
+      '400X250': {
+        width: '400px',
+        height: '250px'
+      },
+      '600X350': {
+        width: '600px',
+        height: '350px'
+      },
+      '700X400': {
+        width: '700px',
+        height: '400px'
+      }
+    };
+    $rootScope.currentTitle = 'Responsive charts';
+    $scope.dimensionType = '600X350';    
+    $scope.containerStyle = {height: '350px', width: '600px'};   
     $scope.myDataSource = {
       "chart": {
         "caption": "Countries With Most Oil Reserves [2017-18]",
@@ -33,6 +50,11 @@
           { "label": "China", "value": "30"}
       ]
     };
+    $scope.buttonClick = function(e){
+      selectedDimension = dimensionMap[e.target.value];
+      $scope.containerStyle.width = selectedDimension.width;
+      $scope.containerStyle.height = selectedDimension.height;
+    }
 
   });
 
