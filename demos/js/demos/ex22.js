08eb0d1d11062070d286d8c002f2241493abe76e
Rahul9046 <rahul.das@fusioncharts.com>
2018-08-20 14:17:43 +0530

    changed chart type from line to pie in change chart type sample

--- demos/js/demos/ex22.js
+++ demos/js/demos/ex22.js
@@ -18,23 +18,30 @@
 
     $scope.myDataSource = {
       "chart": {
-        "caption": "Countries With Most Oil Reserves [2017-18]",
-        "subCaption": "In MMbbl = One Million barrels",
-        "xAxisName": "Country",
-        "yAxisName": "Reserves (MMbbl)",
-        "numberSuffix": "K",
+        "caption": "Recommended Portfolio Split",
+        "subCaption": "For a net-worth of $1M",
+        "showValues": "1",
+        "showPercentInTooltip": "0",
+        "numberPrefix": "$",
+        "enableMultiSlicing": "1",
         "theme": "fusion"
       },
-      "data": [
-          { "label": "Venezuela", "value": "290" },
-          { "label": "Saudi", "value": "260" },
-          { "label": "Canada", "value": "180" },
-          { "label": "Iran", "value": "140" },
-          { "label": "Russia", "value": "115" },
-          { "label": "UAE", "value": "100" },
-          { "label": "US", "value": "30" },
-          { "label": "China", "value": "30"}
-      ]
+      "data": [{
+        "label": "Equity",
+        "value": "300000"
+      }, {
+        "label": "Debt",
+        "value": "230000"
+      }, {
+        "label": "Bullion",
+        "value": "180000"
+      }, {
+        "label": "Real-estate",
+        "value": "270000"
+      }, {
+        "label": "Insurance",
+        "value": "20000"
+      }]
     };
     $scope.initialized = function(chartObj){
       chart = chartObj;
