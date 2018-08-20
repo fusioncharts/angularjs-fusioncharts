284b48ba25c9533e21d63410e0ca67cb528cf1ae
Rahul9046 <rahul.das@fusioncharts.com>
2018-08-20 14:07:15 +0530

    changed add events dynamically event

--- js/demos/ex19.js
+++ js/demos/ex19.js
@@ -11,9 +11,19 @@
 
   app.controller('ex19Controller', function ($scope, $rootScope) {
     $rootScope.demoId = 'ex19';
-    var vm = this;
+    var vm = this,
+    disableStyle = {
+      'border-color': '#d3d3d3',
+      'color': '#d3d3d3',
+      'background': 'none',
+      'cursor': 'default'
+    },
+    activeStyle = {
+      'border-color': '#6957da',
+      'cursor': 'pointer'
+    };
     $rootScope.currentTitle = 'Dynamically add chart event listener';        
-    $scope.message = 'Click on <b>TRACK DATA PLOT CLICK</b>  button to listen to dataplotclick event';
+    $scope.message = 'Click the below buttons to add an event dynamically to the chart';
     $scope.myDataSource = {
       "chart": {
         "caption": "Countries With Most Oil Reserves [2017-18]",
@@ -34,6 +44,8 @@
           { "label": "China", "value": "30"}
       ]
     };
+    $scope.trackButton = activeStyle;
+    $scope.resetButton = disableStyle;
     
     $scope.clickHandler = function(e){
       $scope.$apply(function(){
@@ -43,10 +55,14 @@
     $scope.track = function(){
       FusionCharts.addEventListener('dataplotclick', $scope.clickHandler);
       $scope.message = 'Click on the plot to see the value along with the label';
+      $scope.trackButton = disableStyle;
+      $scope.resetButton = activeStyle;
     };
     $scope.reset = function(){
       FusionCharts.removeEventListener('dataplotclick', $scope.clickHandler);
-      $scope.message = 'Click on <b>TRACK DATA PLOT CLICK</b>  button to listen to dataplotclick event';
+      $scope.message = 'Click the below buttons to add an event dynamically to the chart';
+      $scope.trackButton = activeStyle;
+      $scope.resetButton = disableStyle;
     }
     $scope.$on('$destroy', function() {
       FusionCharts.removeEventListener('dataplotclick', $scope.clickHandler);
