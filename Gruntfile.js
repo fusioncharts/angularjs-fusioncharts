d7377e1d0017f74ab6ae981f70cb36f6223b9d01
Rohan Dey <rohanoid5@gmail.com>
2019-01-03 16:55:36 +0530

    Added example and modified grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,69 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        makeDemos: {
-            demos: {
-                options: {
-                    mainTemplate: 'index.ejs',
-                    out: 'demos'
-                }
-            },
-            gh_pages: {
-                options: {
-                    mainTemplate: 'index.ghpages.ejs',
-                    out: './'
-                }
-            }
-        },
-        uglify: {
-            src: {
-                options: {
-                    banner: '/*! <%= pkg.name %> - v<%= pkg.version %>*/\n\n'
-                },
-                files: {
-                    'dist/angular-fusioncharts.min.js': ['src/angular-fusioncharts.js']
-                }
-            }
-        },
-        copy: {
-            core: {
-                files: [
-					{src:'dist/angular-fusioncharts.min.js', dest: 'demos/js/angular-fusioncharts.min.js'},
-					{src:'src/angular-fusioncharts.js', dest: 'dist/angular-fusioncharts.js'}
-                ]
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    makeDemos: {
+      demos: {
+        options: {
+          mainTemplate: 'index.ejs',
+          out: 'demos'
         }
-    });
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadTasks('./grunt-tasks');
-    //grunt.registerTask('default', ['makeDemos:demos', 'uglify', 'copy']); // For gh-pages branch only
-    grunt.registerTask('default', ['uglify', 'copy']); // For other branches
+      },
+      gh_pages: {
+        options: {
+          mainTemplate: 'index.ghpages.ejs',
+          out: './'
+        }
+      }
+    },
+    uglify: {
+      src: {
+        options: {
+          banner: '/*! <%= pkg.name %> - v<%= pkg.version %>*/\n\n'
+        },
+        files: {
+          'dist/angular-fusioncharts.min.js': ['src/angular-fusioncharts.js']
+        }
+      }
+    },
+    copy: {
+      core: {
+        files: [
+          {
+            src: 'dist/angular-fusioncharts.min.js',
+            dest: 'demos/js/angular-fusioncharts.min.js'
+          },
+          {
+            src: 'src/angular-fusioncharts.js',
+            dest: 'dist/angular-fusioncharts.js'
+          }
+        ]
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      taskName: {
+        files: ['src/*.js', 'example/*.html'],
+        tasks: ['default']
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: './',
+          livereload: true
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadTasks('./grunt-tasks');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  //grunt.registerTask('default', ['makeDemos:demos', 'uglify', 'copy']); // For gh-pages branch only
+  grunt.registerTask('default', ['uglify', 'copy']); // For other branches
+  grunt.registerTask('watch-server', ['connect', 'watch']);
 };
