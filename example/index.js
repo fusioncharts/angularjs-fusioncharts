fbe6b39361f06901e0030d660413e145af421d89
Rohan Dey <rohanoid5@gmail.com>
2019-01-04 16:21:18 +0530

    Refactored example code (#62)
    * Refactored example code
    
    * Fixed gruntfile and example
    
    * Fixed grunt config
    
    * Removed unnecessary code

---
+++ example/index.js
@@ -0,0 +1,65 @@
+var jsonify = res => res.json();
+var dataFetch = fetch(
+  'https://raw.githubusercontent.com/fusioncharts/dev_centre_docs/fusiontime-beta-release/charts-resources/fusiontime/online-sales-single-series/data.json'
+).then(jsonify);
+var schemaFetch = fetch(
+  'https://raw.githubusercontent.com/fusioncharts/dev_centre_docs/fusiontime-beta-release/charts-resources/fusiontime/online-sales-single-series/schema.json'
+).then(jsonify);
+
+var app = angular.module('myApp', ['ng-fusioncharts']);
+
+app.controller('MyController', function($scope) {
+  $scope.timeSeriesDS = {
+    caption: { text: 'Online Sales of a SuperStore in the US' },
+    data: null,
+    yAxis: [
+      {
+        plot: [
+          {
+            value: 'Sales ($)'
+          }
+        ]
+      }
+    ]
+  };
+
+  $scope.columnDS = {
+    chart: {
+      caption: 'Countries With Most Oil Reserves [2017-18]',
+      subCaption: 'In MMbbl = One Million barrels',
+      xAxisName: 'Country',
+      yAxisName: 'Reserves (MMbbl)',
+      numberSuffix: 'K',
+      theme: 'fusion'
+    },
+    data: [
+      { label: 'Venezuela', value: '290' },
+      { label: 'Saudi', value: '260' },
+      { label: 'Canada', value: '180' },
+      { label: 'Iran', value: '140' },
+      { label: 'Russia', value: '115' },
+      { label: 'UAE', value: '100' },
+      { label: 'US', value: '30' },
+      { label: 'China', value: '30' }
+    ]
+  };
+
+  $scope.update = function() {
+    $scope.timeSeriesDS.caption.text = 'Something Else';
+    $scope.columnDS.chart.caption = 'Something Else';
+  };
+
+  Promise.all([dataFetch, schemaFetch]).then(res => {
+    const data = res[0];
+    const schema = res[1];
+    const fusionTable = new FusionCharts.DataStore().createDataTable(
+      data,
+      schema
+    );
+    $scope.$apply(function() {
+      $scope.timeSeriesDS.data = fusionTable;
+    });
+  });
+});
+
+// getData();
